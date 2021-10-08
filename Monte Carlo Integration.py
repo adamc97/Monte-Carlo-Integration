910ff1a4b250fa5bd96b96cb2bc39b4a59bd06a7
adamc97 <77299153+adamc97@users.noreply.github.com>
2021-10-08 13:43:34 +0100

    Add files via upload

--- Monte Carlo Integration.py
+++ Monte Carlo Integration.py
@@ -23,17 +23,20 @@ area_count = 0
 # Set the number of random points tested (the larger this value, the more accurate and less quick the test is)
 n = 10000
 
+# Set the height of the larger shape
+height = 2
+
 # Main MC loop
 # Loop iterated 'n' times, each time random x and y coordinated are set, and if they lie within the function, area_count is increased by 1
 # Regardless of whether the test is successful or not, total_count increases by 1
 # x_coord and y_coord use values based on the integration limits, and the known upper and lower values for the function within the limits 
 while total_count < n:
     x_coord = random.uniform(a, b)
-    y_coord = random.uniform(0, 1) 
+    y_coord = random.uniform(0, height) 
     if y_coord < f(x_coord):
         area_count += 1
     total_count += 1
 
 # The area of the function is the ratio of successful to unsuccessful tests, multiplied by the area of the larger shape (1 * pi)
-result = (area_count / total_count) * np.pi
+result = (area_count / total_count) * (np.pi * height)
 print(result)
